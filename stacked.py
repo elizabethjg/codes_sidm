d4e452f6ffac1d058eb726780bc2a96af00bcfea
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-07-19 16:12:56 -0300

    update

--- stacked.py
+++ stacked.py
@@ -15,6 +15,13 @@ def stack_halos(main_file,path,haloids):
     
     x2d = np.array([])
     y2d = np.array([])
+
+    xr = np.array([])
+    yr = np.array([])
+    zr = np.array([])
+    
+    x2dr = np.array([])
+    y2dr = np.array([])
     
     
     for j in haloids:
@@ -28,17 +35,31 @@ def stack_halos(main_file,path,haloids):
         xrot = (main.a3Dx[j]*X)+(main.a3Dy[j]*Y)+(main.a3Dz[j]*Z);
         yrot = (main.b3Dx[j]*X)+(main.b3Dy[j]*Y)+(main.b3Dz[j]*Z);
         zrot = (main.c3Dx[j]*X)+(main.c3Dy[j]*Y)+(main.c3Dz[j]*Z);
+
+        xrot_r = (main.a3Drx[j]*X)+(main.a3Dry[j]*Y)+(main.a3Drz[j]*Z);
+        yrot_r = (main.b3Drx[j]*X)+(main.b3Dry[j]*Y)+(main.b3Drz[j]*Z);
+        zrot_r = (main.c3Drx[j]*X)+(main.c3Dry[j]*Y)+(main.c3Drz[j]*Z);
         
         x = np.append(x,xrot)
         y = np.append(y,yrot)
         z = np.append(z,zrot)
+
+        xr = np.append(xr,xrot_r)
+        yr = np.append(yr,yrot_r)
+        zr = np.append(zr,zrot_r)
         
         X2d,Y2d = projected_coodinates(X,Y,Z,main.xc_rc[j],main.yc_rc[j],main.zc_rc[j])
         
         x2drot = (main.a2Dx[j]*X2d)+(main.a2Dy[j]*Y2d)
         y2drot = (main.b2Dx[j]*X2d)+(main.b2Dy[j]*Y2d)
+
+        x2drot_r = (main.a2Drx[j]*X2d)+(main.a2Dry[j]*Y2d)
+        y2drot_r = (main.b2Drx[j]*X2d)+(main.b2Dry[j]*Y2d)
     
         x2d = np.append(x2d,x2drot)
         y2d = np.append(y2d,y2drot)
+
+        x2dr = np.append(x2dr,x2drot_r)
+        y2dr = np.append(y2dr,y2drot_r)
         
-    return x,y,z,x2d,y2d
+    return x,y,z,x2d,y2d,xr,yr,zr,x2dr,y2dr
--- stacked_shapes.py
+++ stacked_shapes.py
@@ -10,23 +10,30 @@ zs = ['z0','z51','z96']
 
 z = zs[0]
 
-# path = '/mnt/projects/lensing/SIDM_project/Lentes/Eli_Agus/snapshot_050/SCDM/'
-# main_file = '/home/elizabeth/SIDM/halo_props/halo_props_fof_sidm1_'+z+'_main.csv.bz2'
-path = '/mnt/projects/lensing/SIDM_project/Lentes/Eli_Agus/snapshot_050/CDM/'
-main_file = '/home/elizabeth/SIDM/halo_props/halo_props_fof_cdm_'+z+'_main.csv.bz2'
+path_sidm = '/mnt/projects/lensing/SIDM_project/Lentes/Eli_Agus/snapshot_050/SCDM/'
+main_sidm = '/home/elizabeth/SIDM/halo_props/halo_props_fof_sidm1_'+z+'_main.csv.bz2'
+path_cdm = '/mnt/projects/lensing/SIDM_project/Lentes/Eli_Agus/snapshot_050/CDM/'
+main_cdm = '/home/elizabeth/SIDM/halo_props/halo_props_fof_cdm_'+z+'_main.csv.bz2'
 
 
-main = pd.read_csv(main_file)
+main = pd.read_csv(main_cdm)
+main1 = pd.read_csv(main_sidm)
 
 rc  = np.array(np.sqrt((main.xc - main.xc_rc)**2 + (main.yc - main.yc_rc)**2 + (main.zc - main.zc_rc)**2))
 offset  = rc/main.r_max
 
+rc1  = np.array(np.sqrt((main1.xc - main1.xc_rc)**2 + (main1.yc - main1.yc_rc)**2 + (main1.zc - main1.zc_rc)**2))
+offset1  = rc1/main1.r_max
 
-nhalos = len(main)
+mhalos = (offset < 0.1)*(main.lgM < 14.2)
+mhalos1 = (offset1 < 0.1)*(main1.lgM < 14.2)
 
-haloids = np.array(main.column_halo_id)[offset < 0.1]
+haloids = np.array(main.column_halo_id)[mhalos]
+haloids1 = np.array(main1.column_halo_id)[mhalos1]
     
-x,y,z,x2d,y2d = stack_halos(main_file,path,haloids) 
+x,y,z,x2d,y2d = stack_halos(main_cdm,path_cdm,haloids) 
+x1,y1,z1,x2d1,y2d1 = stack_halos(main_sidm,path_sidm,haloids1) 
+
 
 m3d = (abs(x) < 4.)*(abs(y) < 4.)*(abs(z) < 4.)
 m2d = (abs(x2d) < 4.)*(abs(y2d) < 4.)
@@ -35,3 +42,13 @@ r3d2 = x**2+ y**2 + z**2
 r2d2 = x2d**2+ y2d**2 
 
 v3d,w3d,v2d,w2d = compute_axis(x[m3d],y[m3d],z[m3d],x2d[m2d],y2d[m2d])
+v3dr,w3dr,v2dr,w2dr = compute_axis(x[m3d],y[m3d],z[m3d],x2d[m2d],y2d[m2d],(1./r3d2)[m3d],(1./r2d2)[m2d])
+
+m3d1 = (abs(x1) < 4.)*(abs(y1) < 4.)*(abs(z1) < 4.)
+m2d1 = (abs(x2d1) < 4.)*(abs(y2d1) < 4.)
+
+r3d21 = x1**2+ y1**2 + z1**2
+r2d21 = x2d1**2+ y2d1**2 
+
+v3d1,w3d1,v2d1,w2d1 = compute_axis(x1[m3d1],y1[m3d1],z1[m3d1],x2d1[m2d1],y2d1[m2d1])
+v3dr1,w3dr1,v2dr1,w2dr1 = compute_axis(x1[m3d1],y1[m3d1],z1[m3d1],x2d1[m2d1],y2d1[m2d1],(1./r3d21)[m3d1],(1./r2d21)[m2d1])
