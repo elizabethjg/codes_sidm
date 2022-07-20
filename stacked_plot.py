cc374fc3df3db78c841070d58cb7ed472f993c0d
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-07-20 11:03:03 -0300

    update

--- stacked_plot.py
+++ stacked_plot.py
@@ -26,7 +26,7 @@ nhalos = len(main)
 
 haloids = np.array(main.column_halo_id)[offset < 0.1]
     
-x,y,z,x2d,y2d = stack_halos(main_file,path,haloids,True)   
+x,y,z,x2d,y2d = stack_halos(main_file,path,haloids,False)   
     
 
 f, ax = plt.subplots(1,3, figsize=(12.7,4))
@@ -60,7 +60,8 @@ ax[1].set_ylabel('z [Mpc/h]')
 ax[2].set_xlabel('y [Mpc/h]')
 ax[2].set_ylabel('z [Mpc/h]')
 
-f.savefig('/home/elizabeth/SIDM/coords_sidm1_red.png',bbox_inches='tight')
+# f.savefig('/home/elizabeth/SIDM/coords_sidm1_red.png',bbox_inches='tight')
+f.savefig('/home/elizabeth/SIDM/coords_sidm1.png',bbox_inches='tight')
 # f.savefig('/home/elizabeth/SIDM/coords_cdm.png',bbox_inches='tight')
 
 plt.figure()
@@ -74,5 +75,6 @@ plt.ylim([-4.01,4.01])
 plt.xlabel('x [Mpc/h]')
 plt.ylabel('y [Mpc/h]')
 plt.colorbar()
-plt.savefig('/home/elizabeth/SIDM/coords_sidm1_red_2D.png',bbox_inches='tight')
+# plt.savefig('/home/elizabeth/SIDM/coords_sidm1_red_2D.png',bbox_inches='tight')
+plt.savefig('/home/elizabeth/SIDM/coords_sidm1_2D.png',bbox_inches='tight')
 # plt.savefig('/home/elizabeth/SIDM/coords_cdm_2D.png',bbox_inches='tight')
